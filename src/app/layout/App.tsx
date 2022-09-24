0cde05c7b79a57de49e20af9065ed6b2b68a9b44
alyamr96 <Aly Amr@ALI>
2022-09-24 23:22:11 +0200

    implemented Viewing activity in the card

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -6,7 +6,29 @@ import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 
 function App() {
-  const [activities, setActivities] = useState<Activity[]>([]);
+  var myActivities: Activity[] = [
+    {
+      id: 'myId',
+      title: 'title',
+      date: '1/1/2022',
+      description: 'desc',
+      category: 'cat',
+      city: 'giza',
+      venue: 'sheikh zayed'
+    },
+    {
+      id: 'myId1',
+      title: 'title1',
+      date: '1/2/2022',
+      description: 'desc1',
+      category: 'cat1',
+      city: 'giza1',
+      venue: 'sheikh zayed1'
+    }
+];
+
+  const [activities, setActivities] = useState<Activity[]>(myActivities);
+  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
 
   useEffect(() => {
     axios.get<Activity[]>('https://localhost:7194/api/activities').then(response => {
@@ -14,11 +36,23 @@ function App() {
     })
   }, [])
 
+  function handleSelectActivity(id: string){
+    setSelectedActivity(activities.find(x => x.id === id));
+  }
+
+  function handleCancelSelectActivity(){
+    setSelectedActivity(undefined);
+  }
+
   return (
     <Fragment>
       <NavBar/>
       <Container style={{marginTop: '7em'}}>
-          <ActivityDashboard activities = {activities} />
+          <ActivityDashboard activities = {activities} 
+          selectedActivity = {selectedActivity}
+          selectActivity = {handleSelectActivity}
+          cancelSelectActivity = {handleCancelSelectActivity}
+          />
       </Container>
     </Fragment>
   );
