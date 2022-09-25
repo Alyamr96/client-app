6bc011ffb98a6f91a49713abf98f6a5fdeecbefb
alyamr96 <Aly Amr@ALI>
2022-09-25 20:19:44 +0200

    implement delete activity

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,9 +1,10 @@
 import React, { Fragment, useEffect, useState } from 'react';
 import axios from 'axios';
-import { Container, List } from 'semantic-ui-react';
+import { Container } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+import {v4 as uuid} from 'uuid';
 
 function App() {
   var myActivities: Activity[] = [
@@ -57,9 +58,13 @@ function App() {
   function handleCreateOrEditActivity(activity: Activity){
     activity.id 
       ? setActivities([...activities.filter(x => x.id !== activity.id), activity])
-      : setActivities([...activities, activity]);
+      : setActivities([...activities, {...activity, id: uuid()}]);
       setEditMode(false);
       setSelectedActivity(activity);
+  }
+
+  function handleDeleteActivity(id: string){
+    setActivities([...activities.filter(x => x.id !== id)])
   }
 
   return (
@@ -74,6 +79,7 @@ function App() {
           openForm = {handleFormOpen}
           closeForm = {hanldeFormClose}
           createOrEdit = {handleCreateOrEditActivity}
+          deleteActivity = {handleDeleteActivity}
           />
       </Container>
     </Fragment>
--- src/features/activities/form/ActivityForm.tsx
+++ src/features/activities/form/ActivityForm.tsx
@@ -39,7 +39,7 @@ export default function ActivityForm({activity: selectedActivity, closeForm, cre
                 <Form.Input placeholder='Category' value={activity.category} name='category' onChange={handleInputChange} />
                 <Form.Input placeholder='Date' value={activity.date} name='date' onChange={handleInputChange} />
                 <Form.Input placeholder='City' value={activity.city} name='city' onChange={handleInputChange} />
-                <Form.Input placeholder='Venue' value={activity.venue} name='value' onChange={handleInputChange} />
+                <Form.Input placeholder='Venue' value={activity.venue} name='venue' onChange={handleInputChange} />
                 <Button floated='right' positive type='submit' content='Submit' />
                 <Button onClick={closeForm} floated='right' type='button' content='Cancel' />
             </Form>
